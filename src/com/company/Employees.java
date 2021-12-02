a4a1adef669870fb7be7f50ff359dfcd8f2ba6bb
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-12-02 16:01:05 +0300

    change Employees

--- src/com/company/Employees.java
+++ src/com/company/Employees.java
@@ -2,12 +2,31 @@ package com.company;
 
 public enum Employees {
 
-    WORKER,
-    MANAGER,
-    PROGRAMMER,
-    PRESIDENT
+    PRESIDENT(1, "Президент компании", 100000),
+    MANAGER(2, "Менеджер по пражам", 50000),
+    PROGRAMMER(3, "Разработчик ПО", 90000),
+    WORKER(4, "Рабочий", 40000);
 
     private final int id;
-    private final
+    private final String jobDescription;
+    private final double salary;
 
+    Employees(int id, String jobDescription, double salary){
+        this.id=id;
+        this.jobDescription=jobDescription;
+        this.salary=salary;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Employees employees=Employees.MANAGER;
+        Employees employees2=Employees.PRESIDENT;
+        Employees employees3=Employees.WORKER;
+        Employees employees4=Employees.PROGRAMMER;
+
+        System.out.println(employees.getJobDescription() + " " + employees.getId() + " " +  employees.getSalary());
+        System.out.println(employees2.getJobDescription() + " " + employees2.getId() + " " +  employees2.getSalary());
+        System.out.println(employees3.getJobDescription() + " " + employees3.getId() + " " +  employees3.getSalary());
+        System.out.println(employees4.getJobDescription() + " " + employees4.getId() + " " +  employees4.getSalary());
     }
 }
